4e56130bb125ff8dc6b79204aa5ab1d4c42f65c0
Fu Diwei <fudiwei@sina.com>
2023-08-23 11:45:56 +0800

    fix: vodar.d.ts

--- types/volar.d.ts
+++ types/volar.d.ts
@@ -1,12 +1,16 @@
 ﻿declare module 'vue' {
     export interface GlobalComponents {
-        XNDataTable: (typeof import('@skit/x.naive-ui'))['DataTable'];
+        XNDataTable: (typeof import('@skit/x.naive-ui'))['XNDataTable'];
 
-        XNDropdown: (typeof import('@skit/x.naive-ui'))['Dropdown'];
-        XNDropdownItem: (typeof import('@skit/x.naive-ui'))['DropdownItem'];
-        XNDropdownDivider: (typeof import('@skit/x.naive-ui'))['DropdownDivider'];
+        XNDropdown: (typeof import('@skit/x.naive-ui'))['XNDropdown'];
+        XNDropdownItem: (typeof import('@skit/x.naive-ui'))['XNDropdownItem'];
+        XNDropdownDivider: (typeof import('@skit/x.naive-ui'))['XNDropdownDivider'];
 
-        XNEmpty: (typeof import('@skit/x.naive-ui'))['Empty'];
+        XNSelect: (typeof import('@skit/x.naive-ui'))['XNSelect'];
+        XNSelectOption: (typeof import('@skit/x.naive-ui'))['XNSelectOption'];
+        XNSelectOptionGroup: (typeof import('@skit/x.naive-ui'))['XNSelectOptionGroup'];
+
+        XNEmpty: (typeof import('@skit/x.naive-ui'))['XNEmpty'];
     }
 }
 
